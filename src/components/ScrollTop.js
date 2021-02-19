8838e3ac56cbbfacd231d79dc6b0fb89bd9e66ab
Gokuuu- <60169560+fatahkbm123@users.noreply.github.com>
2021-02-19 18:26:47 +0700

    Update ScrollTop.js

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -3,6 +3,7 @@ import { makeStyles, useScrollTrigger, Zoom } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   root: {
     position: "fixed",
+    zIndex: 999,
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
